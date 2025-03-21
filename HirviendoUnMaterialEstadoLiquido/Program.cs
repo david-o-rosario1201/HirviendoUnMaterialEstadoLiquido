b110ad68d88f1a09ca4c74c7fbcfc82ea66c019d
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-20 23:58:11 -0400

    Add project files.

---
+++ HirviendoUnMaterialEstadoLiquido/Program.cs
@@ -0,0 +1,22 @@
+﻿//Variables
+double diametro = 0;
+double cantidadGas = 0;
+int velocidad = 0; //Posicion de la valvula
+
+
+double caudal = 0;
+double PI = Math.PI;
+
+
+
+// Conversión de pulgadas a metros
+double diametroAMetro = diametro * 0.0254;  // 1 pulgada = 0.0254 metros
+
+// Cálculo del caudal usando el diámetro en metros
+caudal = ((Math.PI * Math.Pow(diametroAMetro, 2)) / 4) * velocidad;
+
+
+Console.WriteLine(caudal);
+
+
+
